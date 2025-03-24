eb9cf1fa86b27b0b7ab2ed6bad074c9dfd703911
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-03-24 18:31:09 -0400

    Initial Commit

---
+++ FriendsGiving/FriendsGivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsGivingApp.swift
+//  FriendsGiving
+//
+//  Created by Noah McGuire on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsGivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
